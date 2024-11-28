ffd5cea6d87f82a1dd1a107eac5726c420dd8048
LuisJURU <98226251+LuisJURU@users.noreply.github.com>
2024-11-28 11:43:03 -0400

    xd

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Admin from "./pages/Admin";
+import Client from "./pages/Client";
+
+function App() {
+  return (
+    <Router>
+        <nav>
+            <Link to="/admin">Administrador</Link>
+            <Link to="/client">Cliente</Link>
+        </nav>
+        <Routes>
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/client" element={<Client />} />
+        </Routes>
+    </Router>
+);
+}
+
+export default App;
